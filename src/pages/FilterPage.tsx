aa988c0bb8610887a47bff031e9a8f34d76ec681
Ririn Nur Fatimah <154777598+RirinnurFatimah@users.noreply.github.com>
2026-02-24 05:56:38 +0700

    Update FilterPage.tsx

--- src/pages/FilterPage.tsx
+++ src/pages/FilterPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import regionData from "../data/indonesia_regions.json";
 
 type Province = { id: number; name: string };
